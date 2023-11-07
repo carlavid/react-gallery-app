cf340125b8bdf446499782caa003b2435287952c
Carla Vidales <carlavidales.ca@gmail.com>
2023-11-06 19:43:11 -0800

    Update nav links

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,23 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
 import axios from 'axios';
 import './App.css';
 import Search from './components/Search';
 import Nav from './components/Nav';
 import PhotoList from './components/PhotoList';
-import NotFound from './components/NotFound';
 import apiKey from './config';
    
 
 const App = () => {
   const [photos, setPhotos] = useState([]);
-  const [query, setQuery] = useState("nature");
+  const [query, setQuery] = useState("cats");
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     setLoading(true);
     let activeFetch = true;
-    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
+      axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
       .then(response => {
         // handle success
         if (activeFetch) {
@@ -29,6 +27,7 @@ const App = () => {
         // handle error
         console.log("Error fetching and parsing data", error);
       });
+    return () => {activeFetch = false}
   }, [query]);
 
   const handleQueryChange = searchText => {
--- vite-project/src/components/Nav.jsx
+++ vite-project/src/components/Nav.jsx
@@ -1,18 +1,13 @@
 import React from "react";
+import { NavLink } from 'react-router-dom';
 
 const Nav = () => {
   return (
     <nav className="main-nav">
       <ul>
-        <li>
-          <a href="#">Cats</a>
-        </li>
-        <li>
-          <a href="#">Dogs</a>
-        </li>
-        <li>
-          <a href="#">Computers</a>
-        </li>
+        <li><NavLink to="cats" end>Cats</NavLink></li>
+        <li><NavLink to="dogs">Dogs</NavLink></li>
+        <li><NavLink to="computers">Computers</NavLink></li>
       </ul>
     </nav>
   );
